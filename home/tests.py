f63351e05fb80c142f6b63d98d87723519a66b6c
aleks10k <zubov0011@gmail.com>
2024-08-04 13:45:15 -0700

    Hard level batchims minor change

--- home/tests.py
+++ home/tests.py
@@ -10,5 +10,5 @@ class ViewsTest(TestCase):
     def test_main(self):
         response = self.client.get(reverse('home:home'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'korean/main.html')
-        self.assertContains(response, '<title>Korean</title>')
+        self.assertTemplateUsed(response, 'home/home.html')
+        self.assertContains(response, '<title>10k apps</title>')
